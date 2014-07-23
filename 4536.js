8d114b78bbbe88028fba68c8baaaca49a7eb2842
fayearth <kakazekage@gmail.com>
2014-07-23 13:08:43 +0700

    Update 4536.js

--- 4536.js
+++ 4536.js
@@ -16,11 +16,11 @@ $(function() {
 		loadTwitter();
 	});
 	$('.displayGoogle').on('click', function(){
-		$(this).after('<div class="g-plusone" data-annotation="inline" data-size="medium" data-width="120"></div>');
+		$(this).after('<div class="g-plusone" data-annotation="bubble" data-height="20" data-href="http://t-arafansindo.blogspot.com/" data-size="medium" data-width="90"/>');
 		loadGooglePlus();
 	});
 	$('.displayFacebook').on('click', function(){
-		$(this).after('<div class="fb-like" data-layout="button_count" data-send="false" data-show-faces="false" data-width="90"></div>');
+		$(this).after('<div class="fb-like" data-href="http://t-arafansindo.blogspot.com/" data-layout="button_count" data-share="false" data-show-faces="false" data-width="90"/>');
 		loadFacebook();
 	});
 });
